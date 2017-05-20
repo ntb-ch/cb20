4de2641c8006eafee17c64ebdcaf1449305b789f
Urs Graf <urs.graf@ntb.ch>
2017-05-20 10:05:49 +0200

    minor corrections

--- Software/make.sh
+++ Software/make.sh
@@ -10,7 +10,10 @@ script_dir="$(dirname $script)"
 
 mkdir -p $flink_build_dir
 pushd $flink_build_dir
-cmake -DCMAKE_TOOLCHAIN_FILE=$toolchain_file -DCMAKE_INSTALL_PREFIX=$install_dir -DCMAKE_BUILD_TYPE=Release $flink_source_dir
+cmake -DCMAKE_TOOLCHAIN_FILE=$toolchain_file \
+      -DCMAKE_INSTALL_PREFIX=$install_dir \
+      -DCMAKE_BUILD_TYPE=Release \
+      $flink_source_dir
 make
 make install
 popd
@@ -18,7 +21,10 @@ popd
 
 mkdir -p $eeros_build_dir
 pushd $eeros_build_dir
-cmake -DCMAKE_TOOLCHAIN_FILE=$toolchain_file -DCMAKE_INSTALL_PREFIX=$install_dir -DCMAKE_BUILD_TYPE=Release $eeros_source_dir
+cmake -DCMAKE_TOOLCHAIN_FILE=$toolchain_file \
+      -DCMAKE_INSTALL_PREFIX=$install_dir \
+      -DCMAKE_BUILD_TYPE=Release \
+      $eeros_source_dir
 make
 make install
 popd
@@ -26,23 +32,21 @@ popd
 
 mkdir -p $flink_eeros_build_dir
 pushd $flink_eeros_build_dir
-cmake -DCMAKE_TOOLCHAIN_FILE=$toolchain_file -DCMAKE_INSTALL_PREFIX=$install_dir \
-      -DADDITIONAL_INCLUDE_DIRS="$install_dir/include" \
-      -DADDITIONAL_LINK_DIRS="$install_dir/lib;$eeros_build_dir/libucl-build/" \
-      -DCMAKE_BUILD_TYPE=Release $flink_eeros_source_dir
+cmake -DCMAKE_TOOLCHAIN_FILE=$toolchain_file \
+      -DCMAKE_INSTALL_PREFIX=$install_dir \
+      -DCMAKE_BUILD_TYPE=Release \
+      $flink_eeros_source_dir
 make
 make install
 popd
-# remove additional inc and lib dirs later
 
 mkdir -p $cb20test_build_dir
 pushd $cb20test_build_dir
-cmake	-DCMAKE_TOOLCHAIN_FILE=$toolchain_file -DCMAKE_INSTALL_PREFIX=$install_dir \
-	-DADDITIONAL_INCLUDE_DIRS="$cb20lib_source_dir/include;$eeros_build_dir/libucl-src/include" \
-	-DADDITIONAL_LINK_DIRS="$cb20lib_build_dir;$eeros_build_dir/libucl-build/" \
+cmake	-DCMAKE_TOOLCHAIN_FILE=$toolchain_file \
+	-DCMAKE_INSTALL_PREFIX=$install_dir \
 	-DCMAKE_BUILD_TYPE=Release \
 	$cb20test_source_dir
 make
 popd
-# remove additional inc and lib dirs later
+
 
